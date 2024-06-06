d59c317aa29a0ef9fd37e492bcb4809e70a04988
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-06-06 07:54:15 -0300

    feat: add list and register router

--- case_angular/src/app/app-routing.module.ts
+++ case_angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: 'register', component: CreateRegistrationComponent },
+  { path: 'list', component: ListItemComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- case_angular/src/app/app.module.ts
+++ case_angular/src/app/app.module.ts
@@ -8,10 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRegistrationComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
