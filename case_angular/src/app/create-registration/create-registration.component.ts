90af27134bf5df4a72a6d9c187c6079e5d33f3ea
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-06-06 13:05:42 -0300

    create select for measure united

--- case_angular/src/app/create-registration/create-registration.component.ts
+++ case_angular/src/app/create-registration/create-registration.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms'
 
+interface Measure {
+  value: string
+  viewValue: string
+}
+
 @Component({
   selector: 'app-create-registration',
   templateUrl: './create-registration.component.html',
   styleUrls: ['./create-registration.component.scss']
 })
+
 export class CreateRegistrationComponent implements OnInit{
   public registerForm!: FormGroup 
+
+  measures: Measure[] = [
+    {value: 'lt', viewValue: 'lt'},
+    {value: 'kg', viewValue: 'kg'},
+    {value: 'un', viewValue: 'un'}
+  ]
 
   constructor(private fb: FormBuilder) {
     
@@ -28,4 +40,6 @@ export class CreateRegistrationComponent implements OnInit{
   saveForm() {
     console.log(this.registerForm.value)
   }
+
+
 }
